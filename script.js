4466b05d33541dcabfc735cd40ba2b09fc4c20e6
JovanGraovac <jovangraovac98@gmail.com>
2024-11-05 13:46:36 +0100

    Added logic for one round

--- script.js
+++ script.js
@@ -13,23 +13,59 @@ function getComputerChoice () {
 
 function getHumanChoice () {
 
-    let humanChoice = prompt("Enter your choice. Rock, Paper or Scissors")
+    let humanInput = prompt("Enter your choice. Rock, Paper or Scissors")
+    let toLowHumanInput = humanInput.toLowerCase()
     
-    if (humanChoice != 'rock' && humanChoice != 'paper' && humanChoice != 'scissors') {
+    if (toLowHumanInput != 'rock' && toLowHumanInput != 'paper' && toLowHumanInput != 'scissors') {
         alert('Your choice is wrong. Enter again');
          return getHumanChoice ()
     }
     
-    if (humanChoice == 'rock') {
+    if (toLowHumanInput == 'rock') {
         return 'rock'
-    } else if (humanChoice == 'paper') {
+    } else if (toLowHumanInput == 'paper') {
         return 'paper';
-    } else if (humanChoice == 'scissors') {
+    } else if (toLowHumanInput == 'scissors') {
         return 'scissors';
-    }
-    
+    }    
 }
 
-console.log(getHumanChoice())
+    let humanScore = 0;
+    let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    
+    if (humanChoice == 'rock' && computerChoice == 'paper') {
+        alert('Computer choice is '+ computerChoice+'. You lose');
+        computerScore++;
+    } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        alert('Computer choice is '+ computerChoice+'. You win');
+        humanScore++;
+    } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+        alert('Computer choice is '+ computerChoice+'. You lose');
+        computerScore++;
+    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        alert('Computer choice is '+ computerChoice+'. You win');
+        humanScore++;
+    } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+        alert('Computer choice is '+ computerChoice+'. You lose');
+        computerScore++;
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        alert('Computer choice is '+ computerChoice+'. You win');
+        humanScore++;
+    } else {
+        alert('Its draw. You both choice ' + computerChoice);
+    }
+}
+
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+
+    console.log(`Human Score is ${humanScore}`);
+    console.log(`Computer Score is ${computerScore}`)
+
+
 
 
