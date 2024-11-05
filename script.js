4dde1f8a3c2ce79db7cef9c87a7fbfe427599437
JovanGraovac <jovangraovac98@gmail.com>
2024-11-05 11:10:51 +0100

    Added logic for human choice

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function getComputerChoice () {
         return 'scissors';
     }
 }
+
+function getHumanChoice () {
+
+    let humanChoice = prompt("Enter your choice. Rock, Paper or Scissors")
+    
+    if (humanChoice != 'rock' && humanChoice != 'paper' && humanChoice != 'scissors') {
+        alert('Your choice is wrong. Enter again');
+         return getHumanChoice ()
+    }
+    
+    if (humanChoice == 'rock') {
+        return 'rock'
+    } else if (humanChoice == 'paper') {
+        return 'paper';
+    } else if (humanChoice == 'scissors') {
+        return 'scissors';
+    }
+    
+}
+
+console.log(getHumanChoice())
+
+
